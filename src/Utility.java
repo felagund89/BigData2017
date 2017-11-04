87ff2fc3e72a784687878a9fb9562dcc56cbbc7e
felagund89 <antonioa89@hotmail.it>
2017-11-04 18:32:26 +0100

    update utility

--- src/Utility.java
+++ src/Utility.java
@@ -54,12 +54,9 @@ public class Utility {
 	
 	public static Map<String, Integer> sortByValue(Map<String, Integer> unsortMap) {
 
-        // 1. Convert Map to List of Map
         List<Map.Entry<String, Integer>> list =
                 new LinkedList<Map.Entry<String, Integer>>(unsortMap.entrySet());
-
-        // 2. Sort list with Collections.sort(), provide a custom Comparator
-        //    Try switch the o1 o2 position for a different order
+      
         Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
             public int compare(Map.Entry<String, Integer> o1,
                                Map.Entry<String, Integer> o2) {
@@ -67,19 +64,10 @@ public class Utility {
             }
         });
 
-        // 3. Loop the sorted list and put it into a new insertion order Map LinkedHashMap
         Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
         for (Map.Entry<String, Integer> entry : list) {
             sortedMap.put(entry.getKey(), entry.getValue());
         }
-
-        /*
-        //classic iterator example
-        for (Iterator<Map.Entry<String, Integer>> it = list.iterator(); it.hasNext(); ) {
-            Map.Entry<String, Integer> entry = it.next();
-            sortedMap.put(entry.getKey(), entry.getValue());
-        }*/
-
 
         return sortedMap;
     }
